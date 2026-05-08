4baa891816ccc18679cb52a8782ca925991dfa72
sekougit <sekoudrame339@gmail.com>
2026-05-08 11:44:18 +0000

    changement agriculture.xlsx et data)loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,21 +1,77 @@
+# import pandas as pd
+# import os
+# from functools import lru_cache
+
+# DATA_PATH = "data"
+
+# def get_all_sectors():
+#     files = os.listdir(DATA_PATH)
+#     return [f.replace(".xlsx", "") for f in files if f.endswith(".xlsx")]
+
+# @lru_cache(maxsize=32)
+# def load_sector_data(sector):
+#     path = f"{DATA_PATH}/{sector}.xlsx"
+#     df = pd.read_excel(path)
+#     df.columns = df.columns.str.strip().str.lower()
+#     return df
+
+# def get_unique_values(df, col):
+#     if col not in df.columns:
+#         return []
+#     return sorted(df[col].dropna().unique())
+
 import pandas as pd
 import os
-from functools import lru_cache
 
 DATA_PATH = "data"
 
-def get_all_sectors():
-    files = os.listdir(DATA_PATH)
-    return [f.replace(".xlsx", "") for f in files if f.endswith(".xlsx")]
 
-@lru_cache(maxsize=32)
+# =========================
+# LISTE DES SECTEURS
+# =========================
+def get_all_sectors():
+
+    files = os.listdir(DATA_PATH)
+
+    return [
+        f.replace(".xlsx", "")
+        for f in files
+        if f.endswith(".xlsx")
+    ]
+
+
+# =========================
+# LOAD DATA
+# =========================
 def load_sector_data(sector):
-    path = f"{DATA_PATH}/{sector}.xlsx"
+
+    path = os.path.join(
+        DATA_PATH,
+        f"{sector}.xlsx"
+    )
+
     df = pd.read_excel(path)
-    df.columns = df.columns.str.strip().str.lower()
+
+    # NORMALISATION COLONNES
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+    )
+
     return df
 
+
+# =========================
+# VALEURS UNIQUES
+# =========================
 def get_unique_values(df, col):
+
     if col not in df.columns:
         return []
-    return sorted(df[col].dropna().unique())
+
+    return sorted(
+        df[col]
+        .dropna()
+        .unique()
+    )
